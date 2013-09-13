cd56a0a076172fa33acd803e79b39ac5aa72695c
Michal Migurski <migurski@ubuntu-gis.(none)>
2013-09-13 19:12:25 +0000

    Added three new datasets (counties, districts, places) and returning pushpin service for all of them

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import request
 from osgeo import ogr, osr
 from shapely import wkb
+import json
 
 app = Flask(__name__)
 
@@ -19,30 +20,41 @@ def areas():
     # This. Is. Python.
     ogr.UseExceptions()
     
-    datasource = ogr.Open('gz_2010_06_140_00_500k/gz_2010_06_140_00_500k.shp')
-    layer = datasource.GetLayer(0)
+    features = []
     point = ogr.Geometry(wkt='POINT(%f %f)' % (lon, lat))
-    
-    defn = layer.GetLayerDefn()
-    names = [defn.GetFieldDefn(i).name for i in range(defn.GetFieldCount())]
-    
-    layer.SetSpatialFilter(point)
-    
-    for feature in layer:
-        
-        properties = dict()
-        
-        for (index, name) in enumerate(names):
-            properties[name] = feature.GetField(index)
-        
-        geometry = feature.GetGeometryRef()
-        shape = wkb.loads(geometry.ExportToWkb())
 
-        feature.DumpReadable()
-        print >> stderr, properties
-        print >> stderr, shape.__geo_interface__
-    
-    return "hello %s" % point
+    #
+    # Look at four files in turn
+    #
+    filenames = [
+        ('Census Counties (2010)', 'gz_2010_us_050_00_500k/gz_2010_us_050_00_500k.shp'),
+        ('Congressional Districts (113th)', 'cb_rd13_us_cd113_500k/cb_rd13_us_cd113_500k.shp'),
+        ('Census Places (2010)', 'gz_2010_06_160_00_500k/gz_2010_06_160_00_500k.shp'),
+        ('Census Tracts (2010)', 'gz_2010_06_140_00_500k/gz_2010_06_140_00_500k.shp'),
+        ]
+
+    for (dataname, filename) in filenames:
+        datasource = ogr.Open(filename)
+        layer = datasource.GetLayer(0)
+        
+        defn = layer.GetLayerDefn()
+        names = [defn.GetFieldDefn(i).name for i in range(defn.GetFieldCount())]
+        
+        layer.SetSpatialFilter(point)
+        
+        for feature in layer:
+            properties = dict(dataset=dataname)
+            
+            for (index, name) in enumerate(names):
+                properties[name] = feature.GetField(index)
+            
+            geometry = feature.GetGeometryRef()
+            shape = wkb.loads(geometry.ExportToWkb())
+            
+            features.append(dict(type='Feature', properties=properties, geometry=shape.__geo_interface__))
+
+    geojson = dict(type='FeatureCollection', features=features)
+    return json.dumps(geojson)
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
