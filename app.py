7fe71f822e621fafa312e5bc0fd5bc978e68e745
Michal Migurski <migurski@ubuntu-gis.(none)>
2013-09-13 18:37:11 +0000

    Added new /areas path and correctly using it to return census tracts

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from sys import stderr
+
 from flask import Flask
+from flask import request
+from osgeo import ogr, osr
 
 app = Flask(__name__)
 
@@ -6,5 +10,26 @@ app = Flask(__name__)
 def hello():
     return 'Hi there'
 
+@app.route("/areas")
+def areas():
+    lat = float(request.args['lat'])
+    lon = float(request.args['lon'])
+    
+    # This. Is. Python.
+    ogr.UseExceptions()
+    
+    datasource = ogr.Open('gz_2010_06_140_00_500k/gz_2010_06_140_00_500k.shp')
+    layer = datasource.GetLayer(0)
+    point = ogr.Geometry(wkt='POINT(%f %f)' % (lon, lat))
+    
+    layer.SetSpatialFilter(point)
+    
+    output = []
+    
+    for feature in layer:
+        feature.DumpReadable()
+    
+    return "hello %s" % point
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
